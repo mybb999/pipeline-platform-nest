510c5c01c2997de3de478edf77a0d089ba339dbc
Lucas <7862318+liujunxiong@user.noreply.gitee.com>
2026-08-10 18:46:01 +0800

    fix: CORS 增加 credentials:true

--- pipeline-platform-server/src/main.ts
+++ pipeline-platform-server/src/main.ts
@@ -9,7 +9,7 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true });
+  app.enableCors({ origin: true, credentials: true });
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
